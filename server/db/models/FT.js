c4f5043e0920b73f14afccf4eb4c3c5f4940b884
Allan <allanwang97@gmail.com>
2022-03-01 15:03:55 -0500

    finish FT model

---
+++ server/db/models/FT.js
@@ -0,0 +1,54 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const FT = db.define('ft', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.NUMERIC,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  artist: {
+    type: Sequelize.STRING,
+    defaultValue: 'Grace Hopper'
+  },
+
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  /*
+  Index
+  0 - Animal
+  1 - hasHat 'Y/N'
+  2 - background color
+  */
+  features: {
+    type: Sequelize.ARRAY(Sequelize. TEXT),
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+
+})
+module.exports(FT)
