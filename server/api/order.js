3dd254dc14c2ff51545315efb77ad2a9a9e45786
Angelo Longoria <arlongoria93@gmail.com>
2022-03-07 16:54:21 -0600

    created routes for order

--- server/api/order.js
+++ server/api/order.js
@@ -4,10 +4,28 @@ const {
 } = require("../db");
 module.exports = router;
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
-    const order = Order.findAll({ where: { userId: id } });
+    const order = await Order.findAll({ where: { userId: id } });
+    res.send(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const order = await Order.Create({ userId: id });
+});
+
+//Filtered Order
+router.get("/currentOrder/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const order = await Order.findAll({
+      where: { userId: id, isFufilled: false },
+    });
     res.send(order);
   } catch (error) {
     next(error);
