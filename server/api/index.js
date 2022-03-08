4c887c14fa339db799be25f7df47d518ccf939b0
Allan <allanwang97@gmail.com>
2022-03-08 12:21:20 -0500

    orderdetail api

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,8 @@ module.exports = router;
 
 router.use('/users', require('./users'));
 router.use('/nft', require('./nft'));
+router.use("/order", require("./order"));
+router.use("/orderDetail", require("./orderDetail"))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found');
