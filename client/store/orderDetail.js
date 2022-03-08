90d41f6386475fda29683c7ac9e08e92a124c560
Angelo Longoria <arlongoria93@gmail.com>
2022-03-08 14:34:09 -0600

    merged main into branch

--- client/store/orderDetail.js
+++ client/store/orderDetail.js
@@ -1,8 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
 // ACTION TYPES
-const GET_ORDER_DETAILS = "GET_CURRENT_DETAILS"
-const CREATE_ORDER_DETAIL = "CREATE_ORDER_DETAILS"
+const GET_ORDER_DETAILS = "GET_CURRENT_DETAILS";
+const CREATE_ORDER_DETAIL = "CREATE_ORDER_DETAILS";
 
 // ACTION CREATORS
 
@@ -10,69 +10,53 @@ const CREATE_ORDER_DETAIL = "CREATE_ORDER_DETAILS"
 const get_orderDetails = (orderDetails) => {
   return {
     type: GET_ORDER_DETAILS,
-    orderDetails
-  }
-}
+    orderDetails,
+  };
+};
 
 //creates new order
 const create_orderDetail = (orderDetail) => {
   return {
     type: CREATE_ORDER_DETAIL,
-    orderDetail
-  }
-}
+    orderDetail,
+  };
+};
 
 //THUNK
 export const getOrderDetails = (orderId) => {
   return async (dispatch) => {
     try {
-      const {data} = await axios.get(`/api/orderDetail/${orderId}`)
-      dispatch(get_orderDetails(data))
-    } catch(error) {
-      console.log('gotOrderDetails Thunk Error: ', error)
+      const { data } = await axios.get(`/api/orderDetail/${orderId}`);
+      dispatch(get_orderDetails(data));
+    } catch (error) {
+      console.log("gotOrderDetails Thunk Error: ", error);
     }
-  }
-}
+  };
+};
 
 export const createOrderDetail = (orderDetail) => {
   return async (dispatch) => {
     try {
-      console.log(orderDetail)
+      // console.log(orderDetail);
       // const {data} = await axios.post(`/api/orderDetail/${orderDetail.orderId}`, orderDetail)
-      const {data} = await axios({
-        method:'post',
+      const { data } = await axios({
+        method: "post",
         url: `/api/orderDetail/${orderDetail.orderId}`,
-        data: orderDetail
-      })
-      dispatch(create_orderDetail(data))
+        data: orderDetail,
+      });
+      dispatch(create_orderDetail(data));
     } catch (error) {
-      console.log('createOrderDetail error: ', error)
+      console.log("createOrderDetail error: ", error);
     }
-  }
-}
+  };
+};
 export default function orderDetailReducer(state = {}, action) {
   switch (action.type) {
     case CREATE_ORDER_DETAIL:
-      return action.orderDetail
+      return action.orderDetail;
     case GET_ORDER_DETAILS:
-      return action.orderDetails
+      return action.orderDetails;
     default:
       return state;
   }
 }
-<<<<<<< HEAD
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Loading complete
-=======
->>>>>>> efa81d6649f2e5665741bd36c4db07b5f6988fc0
--- server/api/orderDetail.js
+++ server/api/orderDetail.js
@@ -8,28 +8,28 @@ module.exports = router;
 router.get("/:orderId", async (req, res, next) => {
   try {
     const orders = await OrderDetail.findAll({
-     where: {
-       orderId: req.params.orderId
-     }
-    })
-    res.send(orders)
-  } catch(error) {
-    next(error)
+      where: {
+        orderId: req.params.orderId,
+      },
+    });
+    res.send(orders);
+  } catch (error) {
+    next(error);
   }
-})
+});
 
 router.post("/:orderId", async (req, res, next) => {
   try {
-    console.log('here')
+    console.log("here");
     res.status(201).send(
       await OrderDetail.create({
         orderId: req.body.orderId,
         nftId: req.body.nftId,
         cost: req.body.cost,
-        quantity: req.body.quantity
+        quantity: req.body.quantity,
       })
-    )
-  } catch (error){
-    next(error)
+    );
+  } catch (error) {
+    next(error);
   }
-})
+});
